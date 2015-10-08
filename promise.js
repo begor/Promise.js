91be193d051651b57fdc14e7e9f0654bb3e8a0b5
Birykov Egor <birykovegor@gmail.com>
2015-10-08 11:45:07 +0300

    First try of a basic module implementation

--- promise.js
+++ promise.js
@@ -1,51 +1,65 @@
 'use strict';
 
-var Promise = function () {
-  this.resolveCallbacks = [];
-  this.rejectCallbacks = [];
-};
+var PromiseModule = (function() {
 
-Promise.prototype = {
-  resolveCallbacks: null,
-  rejectCallbacks: null,
+  var Promise = function () {
+    this.resolveCallbacks = [];
+    this.rejectCallbacks = [];
+  };
 
-  then: function (resolve, reject) {
-    this.resolveCallbacks.push(resolve);
-    if(reject) {
-      this.rejectCallbacks.push(reject);
+  Promise.prototype = {
+    resolveCallbacks: null,
+    rejectCallbacks: null,
+
+    then: function (resolve, reject) {
+      this.resolveCallbacks.push(resolve);
+      if(reject) {
+        this.rejectCallbacks.push(reject);
+      }
     }
-  }
-};
+  };
 
-var Defer = function (promise) {
-  this.promise = promise;
-};
 
-Defer.prototype = {
-  promise: null,
+  var Defer = function (promise) {
+    this.promise = promise;
+  };
 
-  resolve: function (data) {
-    this.promise.resolveCallbacks.forEach(function (callback) {
-        callback(data)
-    });
-  },
+  Defer.prototype = {
+    promise: null,
 
-  reject: function(error) {
-    this.promise.rejectCallbacks.forEach(function (callback) {
-        callback(error)
-    });
-  }
-};
+    resolve: function (data) {
+      this.promise.resolveCallbacks.forEach(function (callback) {
+          callback(data)
+      });
+    },
+
+    reject: function(error) {
+      this.promise.rejectCallbacks.forEach(function (callback) {
+          callback(error)
+      });
+    }
+  };
+
+  return {
+    getDefer: function (promise) {
+      return new Defer(promise);
+    },
+    getPromise: function () {
+      return new Promise()
+    }
+  };
+}());
+
 
 var test = function () {
-  var prom = new Promise();
-  var def = new Defer(prom);
+  var prom = PromiseModule.getPromise();
+  var def = PromiseModule.getDefer(prom);
 
   setTimeout(function() {
     def.resolve("huh");
   }, 1000);
 
   return def.promise;
-}
+};
 
-test().then(function (message) { alert(message); }).then(function () { alert("@@@@@SSSS")});
+test().then(function(msg) { alert(msg)});
